ca3c30b25bb6c09820bc0ad87f89e81a17ebffb9
adva-mo <advamozes@gmail.com>
2023-02-06 20:48:04 +0200

    Established socket connection

--- client/src/components/SocketProvider.js
+++ client/src/components/SocketProvider.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import currentSocket from "../context/socket.context.js";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 function SocketProvider({ children }) {
-  const value = {};
-  return <currentSocket.Provider value={{}}>{children}</currentSocket.Provider>;
+  const value = { socket };
+
+  useEffect(() => {
+    socket.on("users", (data) => {
+      console.log(data);
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <currentSocket.Provider value={value}>{children}</currentSocket.Provider>
+  );
 }
 
 export default SocketProvider;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,23 @@
 require("dotenv").config();
 const mongoose = require("./db/mongoose.js");
+
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+server.listen(3001, () => {
+  console.log("server connected ");
+});
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.broadcast.emit("users", "users");
+});
