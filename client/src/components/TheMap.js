fc58e60a74af9238cd29fc3e8f3661215b9d4242
adva-mo <advamozes@gmail.com>
2023-02-06 19:12:47 +0200

    Established connection to mongoose

--- client/src/components/TheMap.js
+++ client/src/components/TheMap.js
@@ -3,19 +3,7 @@ import currentTrip from "@/context/trip.context";
 import { GoogleMap, DirectionsRenderer, MarkerF } from "@react-google-maps/api";
 import mapStyles from "@/styles/TheMap.module.css";
 import { calculateDistance } from "@/utils/utils";
-
-const gushDanCoordinates = [
-  { lat: 32.067138, lng: 34.772435 },
-  { lat: 32.066039, lng: 34.780807 },
-  { lat: 32.067437, lng: 34.784538 },
-  { lat: 32.073558, lng: 34.779899 },
-  { lat: 32.071531, lng: 34.773976 },
-  { lat: 32.076053, lng: 34.769261 },
-  { lat: 32.083361, lng: 34.772647 },
-  { lat: 32.079307, lng: 34.778278 },
-  { lat: 32.085292, lng: 34.781217 },
-  { lat: 32.081257, lng: 34.787152 },
-];
+import Users from "@/components/Users";
 
 function TheMap({ setLoadedMap }) {
   const {
